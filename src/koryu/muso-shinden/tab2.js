c2cf6c211eb7a50f3f392abf3ccbe96a63bf194f
lei <slayer12135293@hotmail.com>
2017-07-07 02:02:39 +0200

    button scroll to index

--- src/koryu/muso-shinden/tab2.js
+++ src/koryu/muso-shinden/tab2.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { ScrollView, View, Text, StyleSheet, Image, FlatList,Dimensions } from 'react-native'
+import { ScrollView, View, Text, StyleSheet, Image, FlatList,Dimensions, Button } from 'react-native'
 import i18n from '../../i18n/index'
-import { BlockText, KamaeBox } from '../../components'
+import { BlockText, KamaeBox,Touchable } from '../../components'
 import Color from '../../util/color'
 import ScrollableTabView from 'react-native-scrollable-tab-view'
 
@@ -97,8 +97,21 @@ const tab2 = () => {
     ]
 
     return (
-        <View style={styles.container}>   
+        <View style={styles.bodyContainer}>   
+            <View style={{justifyContent: 'space-between', flexDirection: 'row',  alignItems: 'center'}}>
+                <Touchable style={{ width:screenWidth/3 }} onPress={()=>{this.leiList.scrollToIndex({ index:0 })}} > 
+                    <Text style={styles.buttonText}>{'Shoden'}</Text> 
+                </Touchable>
+                <Touchable style={{ width:screenWidth/3 }} onPress={()=>{this.leiList.scrollToIndex({ index:1 })}} >
+                    <Text style={styles.buttonText}>{'Chūden'}</Text> 
+                </Touchable>
+                <Touchable style={{ width:screenWidth/3 }} onPress={()=>{this.leiList.scrollToIndex({ index:1 })}} >
+                    <Text style={styles.buttonText}>{'Chūden'}</Text> 
+                </Touchable>
+            </View>
+
             <FlatList
+                ref={(ref) => { this.leiList = ref }}
                 keyExtractor={x => x.id}
                 data={dataSource}
                 renderItem={({ item })=> {
@@ -108,10 +121,8 @@ const tab2 = () => {
                     }
                 } }
                 horizontal={true}
-            
+                getItemLayout ={(data, index) => ({ length: screenWidth, offset: screenWidth * index, index })}            
             />  
-
-
 
             {/*<ScrollableTabView prerenderingSiblingsNumber ={Infinity} >
                 <View tabLabel='Shoden (初伝)'>
@@ -157,6 +168,11 @@ const StandardRow = (item) =>{
 }
 
 const styles = StyleSheet.create({
+    bodyContainer:{
+        flex: 1,
+        padding:5,
+        alignItems: 'center',
+    },
     container: {
         flex: 1,
         width: screenWidth,
@@ -166,6 +182,10 @@ const styles = StyleSheet.create({
         fontSize:20,
         fontWeight:'bold',
         textAlign: 'center',
+    },
+    buttonText :{
+        textAlign: 'center',
+        fontSize: 15,
     },
 })
 
